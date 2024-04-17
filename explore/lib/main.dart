cb15dbcd5717c5a88c9646123bd0d831bb654324
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-04-17 14:33:33 -0300

    ok
    Ate aqui tudo certo kkk ainda funciona

--- explore/lib/main.dart
+++ explore/lib/main.dart
@@ -8,10 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
+      title: 'Title Section',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter layout demo'),
+          title: const Text('Title Section'),
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -26,21 +26,24 @@ class MyApp extends StatelessWidget {
 
   Widget buildTitleSection() {
     return Container(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(32),
       child: Row(
         children: [
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Título',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
                 Text(
-                  'Subtítulo',
+                  'Kandersteg, Switzerland',
                   style: TextStyle(
                     color: Colors.grey[500],
                   ),
@@ -48,43 +51,41 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(Icons.star, color: Colors.red[500]),
-          Text('41'),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
         ],
       ),
     );
   }
 
   Widget buildButtonSection(BuildContext context) {
-    // Recebendo o contexto como parâmetro
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(context, Icons.call, 'LIGAR'),
-          _buildButtonColumn(context, Icons.near_me, 'ROTA'),
-          _buildButtonColumn(context, Icons.share, 'COMPARTILHAR'),
-        ],
-      ),
+    Color color = Theme.of(context).primaryColor;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
   }
 
-  Column _buildButtonColumn(BuildContext context, IconData icon, String label) {
-    // Recebendo o contexto como parâmetro
-    Color color = Theme.of(context).primaryColor;
-
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Icon(icon, color: color),
         Container(
-          margin: const EdgeInsets.only(top: 8.0),
+          margin: const EdgeInsets.only(top: 8),
           child: Text(
             label,
             style: TextStyle(
-              fontSize: 12.0,
+              fontSize: 12,
               fontWeight: FontWeight.w400,
               color: color,
             ),
